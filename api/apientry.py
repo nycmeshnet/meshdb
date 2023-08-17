1af82f3acb548b4086ee684a1fc8fac51259524a
Quincy Blake <qblakes@gmail.com>
2023-08-17 12:20:02 -0400

    added entrypoint for flask API

---
+++ api/apientry.py
@@ -0,0 +1,21 @@
+from flask import *
+from api import queries
+import json
+
+
+
+app = Flask(__name__)
+app.json.sort_keys = False
+
+
+
+@app.route('/getMembers', methods=['GET'])
+def apiGetMembers():
+    return (queries.getMembers())
+
+@app.route('/getMemberById/<id>', methods=['GET'])
+def apiGetMemberById(id):
+    try:
+        return queries.getMemberByID(id)
+    except:
+        return "Member does not exist", 404
