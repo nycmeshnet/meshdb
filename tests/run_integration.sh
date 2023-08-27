78172bfd409f25587f49cc39dc6e1640272e9eda
Will Nilges <wdn5796@rit.edu>
2023-08-27 01:32:31 -0400

    Add integration test workflow

--- tests/run_integration.sh
+++ tests/run_integration.sh
@@ -4,4 +4,4 @@ set -e
 
 podman build --tag meshdb_integration -f ./tests/Dockerfile .
 
-podman run --rm --network meshdb_api -env-file=.env -it meshdb_integration:latest
+podman run --rm --network meshdb_api --env-file=.env -it meshdb_integration:latest
