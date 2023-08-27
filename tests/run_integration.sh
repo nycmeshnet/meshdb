1cbf7c1ab14e7229efdf33256f6432997f32522f
Will Nilges <wdn5796@rit.edu>
2023-08-27 10:49:13 -0400

    Switch to docker container for networking
    Not sure if there's a way to configure the compose to use the host
    network. Would be nice if we could just mimic this script in the
    configuration. There IS syntax for it.

--- tests/run_integration.sh
+++ tests/run_integration.sh
@@ -2,6 +2,19 @@
 
 set -e
 
-podman build --tag meshdb_integration -f ./tests/Dockerfile .
+ct_cmd=podman
 
-podman run --rm --network meshdb_api --env-file=.env -it meshdb_integration:latest
+while getopts 'd' OPTION; do
+    case "$OPTION" in
+        d)
+            ct_cmd=docker
+            ;;
+        ?)
+            echo "Provide -d to run with docker, otherwise this script uses Podman."
+            ;;
+    esac
+done
+
+$ct_cmd build --tag meshdb_integration -f ./tests/Dockerfile .
+
+$ct_cmd run --rm --network meshdb_api --env-file=.env -it meshdb_integration:latest
