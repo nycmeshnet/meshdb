9d896327da97631d994f19206f063e44ccb3b4cf
Will Nilges <wdn5796@rit.edu>
2023-08-28 22:10:04 -0400

    Add enums to model

--- models/request.py
+++ models/request.py
@@ -1,6 +1,8 @@
+from enum import Enum
 from typing import List
 from typing import Optional
 from sqlalchemy import Column, Integer, DateTime, String, Date, ForeignKey
+from sqlalchemy import Enum as SQLAlchemyEnum
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
@@ -8,12 +10,18 @@ from sqlalchemy.orm import relationship
 import datetime
 from models.baseModel import Base
 
+class RequestStatusEnum(Enum):
+    Open = "Open"
+    Closed = "Closed"
+    Installed = "Installed"
 
 class request(Base):
     __tablename__ = "requests"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    request_status: Mapped[str] = mapped_column(String)
+    request_status: Mapped[RequestStatusEnum] = mapped_column(
+        SQLAlchemyEnum(RequestStatusEnum), nullable=False
+    )
     ticket_id: Mapped[int] = mapped_column()
     member_id: Mapped[int] = mapped_column(ForeignKey("members.id"), nullable=True)
     building_id: Mapped[int] = mapped_column(ForeignKey("buildings.id"), nullable=True)
