bc8b99e6807bdf6b85e0bee0dd93a31b3a9497a6
Will Nilges <wdn5796@rit.edu>
2023-08-29 00:19:29 -0400

    Update env variable names
    To be explicit and call out that these are for meshdb and nothing else
    in the system to avoid collisions and yada yada yada

--- meshdb/db/database.py
+++ meshdb/db/database.py
@@ -9,10 +9,10 @@ def create_db_engine():
     load_dotenv()  # Load .env file (FIXME: Probably move to somewhere less stupid)
     return create_engine(
         "postgresql://{}:{}@{}/{}".format(
-            os.getenv("DB_USER"),
-            os.getenv("DB_PASSWORD"),
-            os.getenv("DB_HOST"),
-            os.getenv("DB_NAME"),
+            os.getenv("MESHDB_DB_USER"),
+            os.getenv("MESHDB_DB_PASSWORD"),
+            os.getenv("MESHDB_DB_HOST"),
+            os.getenv("MESHDB_DB_NAME"),
         ),
         echo=True,
     )
