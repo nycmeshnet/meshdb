ef0ec5f6f9cf573f08444fb764f24af7081ab6ff
Will Nilges <wdn5796@rit.edu>
2023-08-28 22:21:31 -0400

    Connect to the DB in main instead of the API

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from meshdb import app
+from meshdb.db.setup import setup_db
 
 if __name__ == "__main__":
+    print("Configuring DB...")
+    setup_db()
+
     app.run(host=app.config["IP"], port=app.config["PORT"])
 
 application = app
