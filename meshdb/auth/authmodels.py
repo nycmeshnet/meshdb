afb95ca1ba24ed8a724b35d7db9d847c7800273e
Quincy Blake <qblakes@gmail.com>
2023-08-31 18:38:01 -0400

    black magic
    added security config
    
    initial implementation of flask-security
    
    intial implementation of flask-security
    
    removed old auth implementaiton
    
    removed secret keys
    
    restored balance to the universe
    
    Add example of unit testing (#37)
    
    Switch to pyproject.toml & add invoke.py system (#40)
    
    * Switch to pyproject.toml & add invoke.py system
    
    * Fix dependency caching in docker build
    
    * Set line length to 120
    
    Switch GH actions to run pyinvoke lint check
    
    black'd
    
    added flask-security envs
    
    changed email to agnostic entry
    
    added flask-security to dependencies
    
    add comment about csrf
    
    black

---
+++ meshdb/auth/authmodels.py
@@ -0,0 +1,38 @@
+from meshdb.models.baseModel import Base
+from flask_security import UserMixin, RoleMixin, AsaList
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.mutable import MutableList
+from sqlalchemy import Boolean, DateTime, Column, Integer, String, ForeignKey
+import meshdb
+
+
+class RolesUsers(meshdb.db.Model):
+    __tablename__ = "roles_users"
+    id = Column(Integer(), primary_key=True)
+    user_id = Column("user_id", Integer(), ForeignKey("nycmesh_user.id"))
+    role_id = Column("role_id", Integer(), ForeignKey("role.id"))
+
+
+class Role(meshdb.db.Model, RoleMixin):
+    __tablename__ = "role"
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(80), unique=True)
+    description = Column(String(255))
+    permissions = Column(MutableList.as_mutable(AsaList), nullable=True)
+
+
+class User(meshdb.db.Model, UserMixin):
+    __tablename__ = "nycmesh_user"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(255), unique=True)
+    username = Column(String(255), unique=True, nullable=True)
+    password = Column(String(255), nullable=False)
+    last_login_at = Column(DateTime())
+    current_login_at = Column(DateTime())
+    last_login_ip = Column(String(100))
+    current_login_ip = Column(String(100))
+    login_count = Column(Integer)
+    active = Column(Boolean())
+    fs_uniquifier = Column(String(64), unique=True, nullable=False)
+    confirmed_at = Column(DateTime())
+    roles = relationship("Role", secondary="roles_users", backref=backref("users", lazy="dynamic"))
--- meshdb/db/database.py
+++ meshdb/db/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, scoped_session, sessionmaker
 from dotenv import load_dotenv
 import os
 
