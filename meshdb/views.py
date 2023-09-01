9b599b2b6ec5731e310d3d11cd0e7a2a0ed934d8
Will Nilges <wdn5796@rit.edu>
2023-09-01 01:33:32 -0400

    Working? (1/2)

--- meshdb/views.py
+++ meshdb/views.py
@@ -1,17 +1,20 @@
 from flask import current_app as app
 from flask import request
 from flask_security import auth_required
+from flask import Blueprint
+
+route_blueprint = Blueprint("route_blueprint", __name__)
 
 from .db import queries
 
 
-@app.route("/getMembers", methods=["GET"])
+@route_blueprint.route("/getMembers", methods=["GET"])
 @auth_required("token", "session")
 def apiGetMembers():
     return queries.getMembers()
 
 
-@app.route("/getMemberByName", methods=["GET"])
+@route_blueprint.route("/getMemberByName", methods=["GET"])
 def apiGetMemberByName():
     firstName = request.args.get("firstname")
     lastName = request.args.get("lastname")
@@ -19,7 +22,7 @@ def apiGetMemberByName():
     return result
 
 
-@app.route("/getMemberById/<id>", methods=["GET"])
+@route_blueprint.route("/getMemberById/<id>", methods=["GET"])
 def apiGetMemberById(id):
     try:
         return queries.getMemberByID(id)
@@ -27,7 +30,7 @@ def apiGetMemberById(id):
         return "Member not found", 404
 
 
-@app.route("/getMemberDetailsById/<id>", methods=["GET"])
+@route_blueprint.route("/getMemberDetailsById/<id>", methods=["GET"])
 def apiGetMemberDetailsByID(id):
     try:
         return queries.getMemberDetailsByID(id)
@@ -35,7 +38,7 @@ def apiGetMemberDetailsByID(id):
         return "Member not found", 404
 
 
-@app.route("/addMember", methods=["POST"])
+@route_blueprint.route("/addMember", methods=["POST"])
 def apiAddMember():
     try:
         queries.createNewMember(request.get_json())
