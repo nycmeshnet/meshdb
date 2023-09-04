f41433eeae335972586f10c5c4645fccee80daf4
Will Nilges <wdn5796@rit.edu>
2023-09-03 23:07:11 -0400

    Add another test

---
+++ tests/test_getMemberByName.py
@@ -0,0 +1,7 @@
+import pytest
+import requests
+
+def test_getMembers():
+    response = requests.get("http://localhost:8080/getMembers", data={'firstname': 'Daniel', 'lastname' : 'Heredia'})
+    print(response.content)
+    assert response.status_code == 200
