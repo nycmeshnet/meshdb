da6484bb710b0ea5821c1e2e444a47f17e864f73
Will Nilges <wdn5796@rit.edu>
2023-10-25 21:49:14 -0400

    Squash again

--- src/meshapi/migrations/0001_squashed_0015_alter_building_bin.py
+++ src/meshapi/migrations/0001_squashed_0015_alter_building_bin.py
@@ -6,11 +6,6 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-    replaces = [
-        ("meshapi", "0001_squashed_0014_rename_phone_numer_member_phone_number"),
-        ("meshapi", "0015_alter_building_bin"),
-    ]
-
     initial = True
 
     dependencies = [
