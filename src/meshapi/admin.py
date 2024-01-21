1c741b1ace6f2b11ca9281b0fee639a42a713cba
Will Nilges <wdn5796@rit.edu>
2024-01-21 15:27:27 -0500

    Add three basic models, light customization

--- src/meshapi/admin.py
+++ src/meshapi/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.admin.options import forms
+from meshapi.models import Building, Member, Install
+
+admin.site.site_header = "MeshDB Admin"
+admin.site.site_title = "MeshDB Admin Portal"
+admin.site.index_title = "Welcome to MeshDB Admin Portal"
 
 # Register your models here.
+
+#admin.site.register(Building)
+admin.site.register(Member)
+admin.site.register(Install)
+
+class BuildingAdminForm(forms.ModelForm):
+    class Meta:
+        model = Building
+        fields = '__all__'
+        widgets = {
+            'street_address': forms.TextInput(),
+            'city': forms.TextInput(),
+            'state': forms.TextInput(),
+            'zip_code': forms.NumberInput(),
+            'node_name': forms.TextInput(),
+            'street_address': forms.TextInput(),
+        }
+
+@admin.register(Building)
+class BuildingAdmin(admin.ModelAdmin):
+    form = BuildingAdminForm
+
