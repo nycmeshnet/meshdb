2a7148ef8afd5cd1cf4712d1b77451018858e60a
Andrew Dickinson <andrewjd@amazon.com>
2024-01-27 17:04:43 -0500

    Tweak entrypoint celery startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,10 @@ echo 'DB started'
 # It's okay to start Celery in the background and continue without waiting, even though "migrate"
 # might make DB changes we want to notify for since tasks are queued by Django Webhook and
 # are executed as soon as celery starts
+# FIXME: This makes testing locally a bit awkward, since this isn't started by "manage.py runserver"
+#  maybe there's a way to do this better?
 echo 'Staring Celery Worker...'
-celery -A meshdb worker -l INFO &
+celery -A meshdb worker -l INFO --detach
 
 echo 'Running Migrations...'
 python manage.py migrate
