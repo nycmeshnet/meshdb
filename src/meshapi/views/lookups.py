2c4e0a61b3ea3178cdc42ff0f6733be20212bc34
Andrew Dickinson <andrewjd@amazon.com>
2024-01-29 23:16:19 -0500

    Add building lookup endpoint

--- src/meshapi/views/lookups.py
+++ src/meshapi/views/lookups.py
@@ -1,6 +1,6 @@
 from rest_framework import filters, generics, permissions
 
-from meshapi.models import Install, Member
+from meshapi.models import Building, Install, Member
 from meshapi.permissions import IsReadOnly
 from meshapi.serializers import InstallSerializer, MemberSerializer
 
@@ -43,4 +43,30 @@ class LookupInstall(generics.ListAPIView):
             if k in self.search_fields:
                 filter_keyword_arguments_dict[f"{k}__exact"] = v
         queryset = Install.objects.filter(**filter_keyword_arguments_dict)
+        return queryset
+
+
+class LookupBuilding(generics.ListAPIView):
+    search_fields = [
+        "install",
+        "bin",
+        "building_status",
+        "street_address",
+        "city",
+        "state",
+        "zip_code",
+        "primary_nn",
+    ]
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    serializer_class = InstallSerializer
+
+    def get_queryset(self):
+        # TODO: Validate Parameters?
+
+        query_dict = {k: v for k, v in self.request.query_params.items() if v}
+        filter_keyword_arguments_dict = {}
+        for k, v in query_dict.items():
+            if k in self.search_fields:
+                filter_keyword_arguments_dict[f"{k}__exact"] = v
+        queryset = Building.objects.filter(**filter_keyword_arguments_dict)
         return queryset
