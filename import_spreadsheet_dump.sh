d674fd229547c3b5042e93f5ef6597e3d24bc8a9
Willard Nilges <willardnilges@gmail.com>
2024-01-31 21:15:36 -0500

    Variablize Docker PG Command
    I thought it was funny

--- import_spreadsheet_dump.sh
+++ import_spreadsheet_dump.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-PGHOST="127.0.0.1"
-PGPASSWORD="abcd1234" # .env.example password
+DOCKER_PG_COMMAND="docker exec -i meshdb-postgres-1 psql -U meshdb"
 DATA_DIR="./spreadsheet_data/"
 
 set -ex
@@ -35,24 +34,24 @@ num_tables=${#tables[@]}
 # XXX (willnilges): Would it be better to use manage.py?
 for ((i = num_tables - 1; i >= 0; i--));
 do
-	docker exec -i meshdb-postgres-1 psql -U meshdb -c "DROP TABLE IF EXISTS ${tables[i]}"
+	$DOCKER_PG_COMMAND -c "DROP TABLE IF EXISTS ${tables[i]}"
 done
 
 # Import the new data
 for table_name in "${tables[@]}"
 do
-	cat "spreadsheet_data/$table_name.sql" | docker exec -i meshdb-postgres-1 psql -U meshdb
+	cat "spreadsheet_data/$table_name.sql" | $DOCKER_PG_COMMAND
 done
 
 # Fix the numbering
 for table_name in "${tables[@]}"
 do
     if [[ "$table_name" == "meshapi_install" ]]; then
-        max_id=$(($(docker exec -i meshdb-postgres-1 psql -U meshdb -c "SELECT MAX(install_number) FROM $table_name" -At) + 1))
-        docker exec -i meshdb-postgres-1 psql -U meshdb -c "ALTER SEQUENCE "$table_name"_install_number_seq RESTART WITH $max_id"
+        max_id=$(($($DOCKER_PG_COMMAND -c "SELECT MAX(install_number) FROM $table_name" -At) + 1))
+        $DOCKER_PG_COMMAND -c "ALTER SEQUENCE "$table_name"_install_number_seq RESTART WITH $max_id"
     else
-        max_id=$(($(docker exec -i meshdb-postgres-1 psql -U meshdb -c "SELECT MAX(id) FROM $table_name" -At) + 1))
-        docker exec -i meshdb-postgres-1 psql -U meshdb -c "ALTER SEQUENCE "$table_name"_id_seq RESTART WITH $max_id"
+        max_id=$(($($DOCKER_PG_COMMAND -c "SELECT MAX(id) FROM $table_name" -At) + 1))
+        $DOCKER_PG_COMMAND -c "ALTER SEQUENCE "$table_name"_id_seq RESTART WITH $max_id"
     fi
 done
 
