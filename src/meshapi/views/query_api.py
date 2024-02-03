7185f110aefce461e6710d107c3092173e4ab93f
Willard Nilges <willardnilges@gmail.com>
2024-02-02 19:56:53 -0500

    Tweak Query API for Grid Tweaks (#150)
    * Tweak QueryResponse
    
    * update notes field

--- src/meshapi/views/query_api.py
+++ src/meshapi/views/query_api.py
@@ -25,12 +25,14 @@ Line 2 is the same no matter what(tm)
 class QueryResponse:
     install_number: int
     street_address: str
+    unit: str
     city: str
     state: str
     zip_code: int
-    unit: str
     name: str
     email_address: str
+    stripe_email_address: str
+    secondary_emails: list[str]
     notes: str
     network_number: int
     install_status: str
@@ -46,7 +48,9 @@ class QueryResponse:
             unit=install.unit,
             name=install.member.name,
             email_address=install.member.email_address,
-            notes=f"Install Notes: '{install.notes}', Building Notes: '{install.building.notes}', Contact Notes: '{install.member.contact_notes}'",
+            stripe_email_address=install.member.stripe_email_address,
+            secondary_emails=install.member.secondary_emails,
+            notes=f"{install.notes}\n{install.building.notes}\n{install.member.contact_notes}",
             network_number=install.network_number,
             install_status=install.install_status,
         )
