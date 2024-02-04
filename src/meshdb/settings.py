3f09764b9530d2e9e9ad60ade25ce6a82d7eeea6
Andrew Dickinson <andrewjd@amazon.com>
2024-02-03 19:21:04 -0500

    Add favicons to meshdb and meshweb apps

--- src/meshdb/settings.py
+++ src/meshdb/settings.py
@@ -106,7 +106,7 @@ ROOT_URLCONF = "meshdb.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "meshdb/templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
