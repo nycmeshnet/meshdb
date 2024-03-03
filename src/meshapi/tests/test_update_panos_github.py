fc8a35733a17f39c90c3b27315d9f0d526a10a69
Willard Nilges <willardnilges@gmail.com>
2024-03-03 00:30:01 -0500

    fine I'll move it to another fucking secret

--- src/meshapi/tests/test_update_panos_github.py
+++ src/meshapi/tests/test_update_panos_github.py
@@ -52,6 +52,10 @@ class TestPanoUtils(TestCase):
         self.branch = os.environ.get("PANO_BRANCH")
         self.directory = os.environ.get("PANO_DIR")
         self.host_url = os.environ.get("PANO_HOST")
+        self.token = os.environ.get("GITHUB_TOKEN")
+
+        if not self.owner or not self.repo or not self.branch or not self.directory or not self.host_url or not self.token:
+            return Response({"detail": "Did not find environment variables"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def test_parse_pano_title(self):
         test_cases = {
