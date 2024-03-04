3a45c1827efe7c368c5d99fc107a8c103b8048ff
Willard Nilges <willardnilges@gmail.com>
2024-03-03 19:37:15 -0500

    Move HTML into template (properly)

--- src/meshapi/widgets.py
+++ src/meshapi/widgets.py
@@ -1,26 +1,17 @@
 from django.forms import Widget
+from django.template import loader
+from django.utils.safestring import mark_safe
 
 class PanoramaViewer(Widget):
     template_name="widgets/panorama_viewer.html"
-    
+
+    def get_context(self, name, value, attrs=None):
+        return {'widget': {
+            'name': name,
+            'value': value.split(","),
+        }}
+
     def render(self, name, value, attrs=None, renderer=None):
-        # Get the value of the model field
-        model_field_value = self.attrs.get('panoramas', None)
-        print(f"bro wtf: {self.attrs}")
-        print(f"bro wtf: {name}")
-        print(f"bro wtf: {value}")
-
-        # Now you can use the model_field_value as needed
-        # For example, you can use it to modify the rendering of the widget
-        if value:
-            # Do something with model_field_value
-            back = "<div style='display:flex; flex-direction: row; width:100%; overflow-x:auto; max-height:300px;'>"
-            panos = value.split(',')
-            for p in panos:
-                back += f"<img src='{p}' height='300px'/>"
-            back += "</div>"
-            return back
-
-        # Call the parent class' render method
-        return super().render(name, value, attrs, renderer)
-
+        context = self.get_context(name, value, attrs)
+        template = loader.get_template(self.template_name).render(context)
+        return mark_safe(template)
