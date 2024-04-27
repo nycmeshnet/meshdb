e6ed91cfd1a28b8a427fe88fdc9d334de8705c59
Richard Yang <33008397+vivinano@users.noreply.github.com>
2024-04-27 02:17:25 -0400

    Update settings.py

--- src/meshdb/settings.py
+++ src/meshdb/settings.py
@@ -105,6 +105,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "meshdb.middleware.HandleServerErrorMiddleware",
 ]
 
 
