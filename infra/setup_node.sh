b0082a68bc5865148202dff0c920833758b228f9
James Otten <jamesotten1@gmail.com>
2024-05-03 21:00:10 -0400

    typo

--- infra/setup_node.sh
+++ infra/setup_node.sh
@@ -5,4 +5,4 @@ NODE_TOKEN="$(cat /var/lib/rancher/k3s/server/node-token)"
 
 target_host="$1"
 
-ssh -t ubuntu@$target_host "curl -sfL https://get.k3s.io>k3s; sudo bash k3s --server https://${MASTER_IP}:6443 --token $NODE_TOKEN;sudo apt-get update && sudo apt-get install nfs-common -y"0
+ssh -t ubuntu@$target_host "curl -sfL https://get.k3s.io>k3s; sudo bash k3s --server https://${MASTER_IP}:6443 --token $NODE_TOKEN;sudo apt-get update && sudo apt-get install nfs-common -y"
