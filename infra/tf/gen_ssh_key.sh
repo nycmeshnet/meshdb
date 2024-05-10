ef3120ae011ee2421549ccf95adec10a3924bc54
james-otten <james-otten@users.noreply.github.com>
2024-05-09 22:27:13 -0400

    Update infra/tf/gen_ssh_key.sh
    Co-authored-by: Willard Nilges <willardnilges@gmail.com>

--- infra/tf/gen_ssh_key.sh
+++ infra/tf/gen_ssh_key.sh
@@ -3,7 +3,7 @@
 ENV_NAME="$1"
 EXPECTED_KEYFILE_NAME="./meshdb$ENV_NAME"
 
-if [ ! -f "$EXPECTED_KEYFILE_NAME" ];
+if [ ! -f "$EXPECTED_KEYFILE_NAME" ]; then
     ssh-keygen -t ed25519 -f $EXPECTED_KEYFILE_NAME -C meshdb${ENV_NAME}@db.mesh.nycmesh.net -q -N ""
 else
     echo "File $EXPECTED_KEYFILE_NAME already exists"
