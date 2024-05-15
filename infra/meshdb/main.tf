42875bb8f77837a5f0d78ae8fefa27ccf59dacb2
Willard Nilges <willardnilges@gmail.com>
2024-05-14 22:20:10 -0400

    First attempt at locally rendering

--- infra/meshdb/main.tf
+++ infra/meshdb/main.tf
@@ -10,17 +10,24 @@ resource "kubernetes_namespace" "meshdb-ns" {
 }
 
 # Generate the chart
-resource "null_resource" "generate_meshdb_chart" {
+#resource "null_resource" "generate_meshdb_chart" {
+#  provisioner "local-exec" {
+#    command = "cd ${path.module}/../helm/meshdb/; helm tempate . -f values.yaml -f secret.values.yaml > meshdb.yaml"
+#  }
+#}
+
+# TODO: template version
+resource "null_resource" "package_meshdb_chart" {
   provisioner "local-exec" {
-    command = "cd ${path.module}/../helm/meshdb/; helm tempate . -f values.yaml -f secret.values.yaml > meshdb.yaml"
+    command = "cd ${path.module}/../helm/; helm package meshdb; mv ${path.module}/../helm/meshdb-0.1.0.tgz ${path.module}/../meshdb/"
   }
 }
 
 # Create meshdb with the manifest
 resource "kubernetes_manifest" "meshdb" {
-  manifest = yamldecode(file("${path.module}/../helm/meshdb/meshdb.yaml"))
+  manifest = yamldecode(file("${path.module}/meshdb.yaml"))
   depends_on = [
     kubernetes_namespace.meshdb-ns,
-    null_resource.generate_meshdb_chart
+    null_resource.package_meshdb_chart
   ]
 }
