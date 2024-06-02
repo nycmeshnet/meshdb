d7aa2a91b361499ecfcff88b96bf6a790e077869
Andrew Dickinson <andrewjd@amazon.com>
2024-06-02 16:23:54 -0400

    Integrate django-import-export

--- src/meshdb/settings.py
+++ src/meshdb/settings.py
@@ -94,6 +94,7 @@ INSTALLED_APPS = [
     "django_filters",
     "django_jsonform",
     "dbbackup",
+    "import_export",
 ]
 
 MIDDLEWARE = [
@@ -299,3 +300,6 @@ SPECTACULAR_SETTINGS = {
         "docExpansion": "none",
     },
 }
+
+IMPORT_EXPORT_IMPORT_PERMISSION_CODE = "add"
+IMPORT_EXPORT_EXPORT_PERMISSION_CODE = "view"
