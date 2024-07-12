791303f10f9973e1e3455b1c492b20f6d1bb8a32
harsha-mangena <harsha.mangena@gmail.com>
2024-07-12 17:57:31 -0400

    refactor migration

--- src/meshapi/migrations/0010_member_payment_preference.py
+++ src/meshapi/migrations/0010_member_payment_preference.py
@@ -1,24 +1,23 @@
 from django.db import migrations, models
 
-class PaymentPreference(models.TextChoices):
-    CASH = "cash", "Cash"
-    STRIPE = "stripe", "Stripe"
-    NONE = None, "None"
-
 class Migration(migrations.Migration):
     dependencies = [
         ("meshapi", "0009_member_additional_phone_numbers_alter_member_phone_number"),
-    ]
 
     operations = [
-        migrations.AlterField(
+        migrations.AddField(
             model_name="member",
             name="payment_preference",
             field=models.CharField(
-                max_length=6,
-                choices=PaymentPreference.choices,
-                default=PaymentPreference.NONE,
+                choices=[
+                    ("cash", "Cash"),
+                    ("stripe", "Stripe"),
+                    (None, "None"),
+                ],
+                default=None,
                 help_text="Preferred payment method for this member",
+                null=True,
+                blank=True,
             ),
         ),
     ]
