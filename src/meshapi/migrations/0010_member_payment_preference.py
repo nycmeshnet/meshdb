8eb4da7784d794a00548f45ba1937f8509d4a6b4
harsha-mangena <harsha.mangena@gmail.com>
2024-07-15 11:34:30 -0400

    typo fix

--- src/meshapi/migrations/0010_member_payment_preference.py
+++ src/meshapi/migrations/0010_member_payment_preference.py
@@ -3,6 +3,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
     dependencies = [
         ("meshapi", "0009_member_additional_phone_numbers_alter_member_phone_number"),
+    ]
 
     operations = [
         migrations.AddField(
