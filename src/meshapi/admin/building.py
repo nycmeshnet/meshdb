04fb114aa4e54f21073a4a7bd0d0922cd7f78f4c
Andrew Dickinson <andrewjd@amazon.com>
2024-07-30 00:04:22 -0400

    Remove TODO that's already done

--- src/meshapi/admin/building.py
+++ src/meshapi/admin/building.py
@@ -51,7 +51,6 @@ class BuildingAdminForm(forms.ModelForm):
 
 @admin.register(Building)
 class BuildingAdmin(ImportExportModelAdmin, ExportActionMixin):
-    # TODO: Add inline here to show the LOSes this building has
     form = BuildingAdminForm
     list_display = ["__str__", "street_address", "primary_node"]
     search_fields = [
