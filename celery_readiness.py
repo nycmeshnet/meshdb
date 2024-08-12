7febff4b4b3e91f66eea181ab3918d65b08fcaeb
Willard Nilges <willardnilges@gmail.com>
2024-08-11 23:25:03 -0400

    oops

--- celery_readiness.py
+++ celery_readiness.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-READINESS_FILE = Path('/tmp/celery_ready')
+READINESS_FILE = Path('/tmp/celery_worker_ready')
 
 if not READINESS_FILE.is_file():
     sys.exit(1)
