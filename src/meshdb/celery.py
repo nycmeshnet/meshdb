55caa75a67a58ccdd8abbca99a06d6758c33be88
Willard Nilges <willardnilges@gmail.com>
2024-08-13 20:19:15 -0400

    lint

--- src/meshdb/celery.py
+++ src/meshdb/celery.py
@@ -2,13 +2,14 @@ import os
 from pathlib import Path
 
 from celery import Celery, bootsteps
-from celery.signals import beat_init, worker_ready, worker_shutdown
 from celery.apps.worker import Worker
+from celery.signals import beat_init, worker_ready, worker_shutdown
 from dotenv import load_dotenv
 
 HEARTBEAT_FILE = Path("/tmp/celery_worker_heartbeat")
 READINESS_FILE = Path("/tmp/celery_worker_ready")
 BEAT_READINESS_FILE = Path("/tmp/celery_beat_ready")
+
 
 # This is still somewhat contentious in the Celery community, but the popular
 # opinion seems to be this approach:
@@ -18,11 +19,11 @@ BEAT_READINESS_FILE = Path("/tmp/celery_beat_ready")
 class LivenessProbe(bootsteps.StartStopStep):
     requires = {"celery.worker.components:Timer"}
 
-    def __init__(self, parent: Worker, **kwargs: dict):
-        self.requests = []
+    def __init__(self, parent: Worker, **kwargs: dict) -> None:
+        self.requests: list = []
         self.tref = None
 
-    def start(self, parent: Worker):
+    def start(self, parent: Worker) -> None:
         self.tref = parent.timer.call_repeatedly(
             1.0,
             self.update_heartbeat_file,
@@ -30,25 +31,25 @@ class LivenessProbe(bootsteps.StartStopStep):
             priority=10,
         )
 
-    def stop(self, parent: Worker):
+    def stop(self, parent: Worker) -> None:
         HEARTBEAT_FILE.unlink(missing_ok=True)
 
-    def update_heartbeat_file(self, parent: Worker):
+    def update_heartbeat_file(self, parent: Worker) -> None:
         HEARTBEAT_FILE.touch()
 
 
 @worker_ready.connect  # type: ignore[no-redef]
-def worker_ready(**_: dict):
+def worker_ready(**_: dict) -> None:
     READINESS_FILE.touch()
 
 
 @worker_shutdown.connect  # type: ignore[no-redef]
-def worker_shutdown(**_: dict):
+def worker_shutdown(**_: dict) -> None:
     READINESS_FILE.unlink(missing_ok=True)
 
 
 @beat_init.connect
-def beat_ready(**_: dict):
+def beat_ready(**_: dict) -> None:
     BEAT_READINESS_FILE.touch()
 
 
