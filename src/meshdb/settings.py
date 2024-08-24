9f51cc8b3df0a7f77f39e1c84d39c06894f8a04c
james-otten <james-otten@users.noreply.github.com>
2024-08-23 21:11:40 -0400

    Set urls from env + cleanup (#483)
    * set urls from env + cleanup
    
    * lint
    
    * make individual env vars
    
    * make individual env vars

--- src/meshdb/settings.py
+++ src/meshdb/settings.py
@@ -36,6 +36,11 @@ PROFILING_ENABLED = DEBUG and not os.environ.get("DISABLE_PROFILING", "False") =
 
 USE_X_FORWARDED_HOST = True
 
+LOS_URL = os.environ.get("LOS_URL", "https://devlos.mesh.nycmesh.net")
+MAP_URL = os.environ.get("MAP_BASE_URL", "https://devmap.mesh.nycmesh.net")
+PG_ADMIN_URL = os.environ.get("PG_ADMIN_URL", "/pgadmin/")
+FORMS_URL = os.environ.get("FORMS_URL", "https://devforms.mesh.nycmesh.net")
+
 ALLOWED_HOSTS = [
     "db.grandsvc.mesh.nycmesh.net",
     "db.grandsvc.mesh",
