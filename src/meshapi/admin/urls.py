353073b6a1f53044c767ebba27369cc653c8897f
Andrew Dickinson <andrewjd@amazon.com>
2024-08-28 12:18:10 -0400

    Fix admin/admin/ in the URLs

--- src/meshapi/admin/urls.py
+++ src/meshapi/admin/urls.py
@@ -9,9 +9,9 @@ from meshapi.admin.password_reset import (
 )
 
 urlpatterns = [
-    path("admin/password_reset/", AdminPasswordResetView.as_view(), name="admin_password_reset"),
-    path("admin/password_reset/done/", AdminPasswordResetDoneView.as_view(), name="password_reset_done"),
-    path("reset/<uidb64>/<token>/", AdminPasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path("reset/done/", AdminPasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path("password_reset/", AdminPasswordResetView.as_view(), name="admin_password_reset"),
+    path("password_reset/done/", AdminPasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("password_reset/<uidb64>/<token>/", AdminPasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("password_reset/done/", AdminPasswordResetCompleteView.as_view(), name="password_reset_complete"),
     path("", admin.site.urls),
 ]
