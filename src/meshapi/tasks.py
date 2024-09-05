ab8812fee5005936026e0073ef002ab1cdec2177
Willard Nilges <willardnilges@gmail.com>
2024-09-04 21:25:57 -0400

    Stub reset task

--- src/meshapi/tasks.py
+++ src/meshapi/tasks.py
@@ -16,6 +16,11 @@ from meshdb.celery import app as celery_app
 
 @celery_app.task
 def run_database_backup() -> None:
+    # Don't run a backup unless it's prod1
+    if os.environ.get("MESHDB_ENVIRONMENT") != "prod1":
+        logging.warn(f"Not running database backup. This environment is: \"{os.environ.get("MESHDB_ENVIRONMENT", "")}\"")
+        return
+
     logging.info("Running database backup task")
     if not os.environ.get("AWS_ACCESS_KEY_ID") or not os.environ.get("AWS_SECRET_ACCESS_KEY"):
         logging.error("Could not run backup. Missing AWS credentials!")
@@ -23,6 +28,27 @@ def run_database_backup() -> None:
 
     try:
         management.call_command("dbbackup")
+    except Exception as e:
+        logging.exception(e)
+        raise e
+
+@celery_app.task
+def reset_dev_database() -> None:
+    # Only reset dev environments (very important!)
+    if "dev" not in os.environ.get("MESHDB_ENVIRONMENT", ""):
+        logging.warn(f"Not resetting this environment. This environment is: \"{os.environ.get("MESHDB_ENVIRONMENT", "")}\"")
+        return
+
+    logging.info("Running database reset task")
+
+    if not os.environ.get("AWS_ACCESS_KEY_ID") or not os.environ.get("AWS_SECRET_ACCESS_KEY"):
+        logging.error("Could not run backup. Missing AWS credentials!")
+        return
+
+    latest_backup = ""
+
+    try:
+        management.call_command("dbrestore", "-i", latest_backup)
     except Exception as e:
         logging.exception(e)
         raise e
@@ -64,5 +90,9 @@ celery_app.conf.beat_schedule = {
     "run-database-backup-hourly": {
         "task": "meshapi.tasks.run_database_backup",
         "schedule": crontab(minute="20", hour="*/1"),
+    },
+    "run-reset-dev-database-daily": {
+        "task": "meshapi.tasks.run_database_backup",
+        "schedule": crontab(minute="0", hour="0"),
     },
 }
