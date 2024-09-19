b3592661b879e42cc3caef7600d4e9d26d46062b
Andrew Dickinson <andrewjd@amazon.com>
2024-09-18 23:04:53 -0400

    Change ticket number to charfield

--- src/meshapi/models/install.py
+++ src/meshapi/models/install.py
@@ -53,10 +53,12 @@ class Install(models.Model):
     )
 
     # OSTicket Ticket Number
-    ticket_number = models.IntegerField(
+    ticket_number = models.CharField(
         blank=True,
         null=True,
-        help_text="The ticket number of the OSTicket used to track communications with the member about this install",
+        help_text="The ticket number of the OSTicket used to track communications with the member about "
+        "this install. Note that although this appears to be an integer, it is not. Leading zeros"
+        "are important, so this should be stored as a string at all times",
     )
 
     # Important dates
