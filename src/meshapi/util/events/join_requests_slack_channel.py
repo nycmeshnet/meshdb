332c7e566d16328cfe9c8ad11a34e80041875115
Andrew Dickinson <andrewjd@amazon.com>
2024-09-23 23:11:20 -0400

    Fix url typo

--- src/meshapi/util/events/join_requests_slack_channel.py
+++ src/meshapi/util/events/join_requests_slack_channel.py
@@ -30,7 +30,7 @@ def send_join_request_slack_message(sender: ModelBase, instance: Install, create
     response = requests.post(
         SLACK_JOIN_REQUESTS_CHANNEL_WEBHOOK_URL,
         json={
-            "text": f"*<https://www.nycmesh.net/map/nodes/{install.install_number})"
+            "text": f"*<https://www.nycmesh.net/map/nodes/{install.install_number}"
             f"|{install.one_line_complete_address}>*\n"
             f"{building_height} · {roof_access} · No LoS Data Available"
         },
