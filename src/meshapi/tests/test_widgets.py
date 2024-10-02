69a0602f821331d00fd0be115628af3f6c8db88f
Willard Nilges <willardnilges@gmail.com>
2024-10-02 19:35:36 -0400

    what the fuck

--- src/meshapi/tests/test_widgets.py
+++ src/meshapi/tests/test_widgets.py
@@ -8,8 +8,8 @@ class TestPanoramaViewer(TestCase):
         pass
 
     def test_pano_get_context(self):
-        PanoramaViewer.pano_get_context("test", "[\"blah\", \"blah2\"]")
+        PanoramaViewer.pano_get_context("test", '["blah", "blah2"]')
 
     def test_pano_get_context_bad_value(self):
-        PanoramaViewer.pano_get_context("test", 100) # type: ignore
-        PanoramaViewer.pano_get_context("test", None) # type: ignore
+        PanoramaViewer.pano_get_context("test", 100)  # type: ignore
+        PanoramaViewer.pano_get_context("test", None)  # type: ignore
