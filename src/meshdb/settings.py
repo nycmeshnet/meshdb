76b14a9dfbdf92284a86ea6538019ed742c07ff2
james-otten <james-otten@users.noreply.github.com>
2024-11-09 12:33:55 -0500

    update wiki domain (#702)

--- src/meshdb/settings.py
+++ src/meshdb/settings.py
@@ -446,7 +446,7 @@ SPECTACULAR_SETTINGS = {
         {
             "name": "Sectors",
             "description": 'Special devices with antennas with broad coverage of a radial "slice" of land area. '
-            "See https://wiki.mesh.nycmesh.net/books/3-hardware-firmware/page/ubiquiti-liteap-sector#bkmrk-page-title",
+            "See https://wiki.nycmesh.net/books/3-hardware-firmware/page/ubiquiti-liteap-sector#bkmrk-page-title",
         },
         {
             "name": "Access Points",
