12715258083264d7ab996a5c0a9918032659a8c6
Andrew Dickinson <andrewjd@amazon.com>
2024-11-22 20:36:03 -0500

    Move javascript date fields outside of map serializers

--- src/meshapi/serializers/map.py
+++ src/meshapi/serializers/map.py
@@ -1,58 +1,17 @@
-import datetime
 import os
 from collections import OrderedDict
-from datetime import timezone
 from typing import List, Optional, Tuple
 from urllib.parse import urlparse
 
-from drf_spectacular.types import OpenApiTypes
-from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 
 from meshapi.models import Device, Install, Link, Node, Sector
+from meshapi.serializers.javascript_date_field import JavascriptDateField, JavascriptDatetimeField
 
 EXCLUDED_INSTALL_STATUSES = {
     Install.InstallStatus.CLOSED,
     Install.InstallStatus.NN_REASSIGNED,
 }
-
-
-@extend_schema_field(OpenApiTypes.INT)
-class JavascriptDatetimeField(serializers.Field):
-    def to_internal_dt(self, datetime_int_val: Optional[int]) -> Optional[datetime.datetime]:
-        if datetime_int_val is None:
-            return None
-
-        return datetime.datetime.fromtimestamp(datetime_int_val / 1000)
-
-    def to_internal_value(self, date_int_val: Optional[int]) -> Optional[datetime.datetime]:
-        return self.to_internal_dt(date_int_val)
-
-    def dt_to_representation(self, datetime_val: datetime.datetime) -> Optional[int]:
-        if datetime_val is None:
-            return None
-
-        return int(datetime_val.timestamp() * 1000)
-
-    def to_representation(self, datetime_val: datetime.datetime) -> Optional[int]:
-        return self.dt_to_representation(datetime_val)
-
-
-@extend_schema_field(OpenApiTypes.INT)
-class JavascriptDateField(JavascriptDatetimeField):
-    def to_internal_value(self, date_int_val: Optional[int]) -> Optional[datetime.date]:
-        return self.to_internal_dt(date_int_val).date()
-
-    def to_representation(self, date_val: datetime.date) -> Optional[int]:
-        if date_val is None:
-            return None
-
-        return self.dt_to_representation(
-            datetime.datetime.combine(
-                date_val,
-                datetime.datetime.min.time(),
-            ).astimezone(timezone.utc)
-        )
 
 
 class MapDataInstallSerializer(serializers.ModelSerializer):
