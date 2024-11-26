9b71c72bca1ded3a468971e7ed7caa74c04f4a1b
Andrew Dickinson <andrewjd@amazon.com>
2024-11-25 20:54:36 -0500

    Add tests

--- src/meshapi/tests/test_install_create_signals.py
+++ src/meshapi/tests/test_install_create_signals.py
@@ -5,7 +5,7 @@ import requests_mock
 from django.test import TestCase
 from flags.state import disable_flag, enable_flag
 
-from meshapi.models import Building, Install, Member
+from meshapi.models import Building, Install, Member, Node
 from meshapi.tests.sample_data import sample_building, sample_install, sample_member
 
 
@@ -91,6 +91,61 @@ class TestInstallCreateSignals(TestCase):
                 "phone": "+1 555-555-5555",
                 "location": "3333 Chom St, Brooklyn NY, 11111",
                 "rooftop": "Rooftop install",
+                "existingNetworkNumber": "",
+                "ncl": True,
+                "ip": "*.*.*.*",
+                "locale": "en",
+            },
+        )
+
+        install.refresh_from_db()
+        self.assertEqual(install.ticket_number, "00123456")
+
+    @patch(
+        "meshapi.util.events.osticket_creation.OSTICKET_NEW_TICKET_ENDPOINT",
+        "http://example.com/test-url",
+    )
+    @patch(
+        "meshapi.util.events.osticket_creation.OSTICKET_API_TOKEN",
+        "mock-token",
+    )
+    @requests_mock.Mocker()
+    def test_constructing_install_triggers_osticket_for_existing_node(self, request_mocker):
+        request_mocker.post("http://example.com/test-url", text="00123456", status_code=201)
+
+        disable_flag("INTEGRATION_ENABLED_SEND_JOIN_REQUEST_SLACK_MESSAGES")
+        enable_flag("INTEGRATION_ENABLED_CREATE_OSTICKET_TICKETS")
+
+        node = Node(
+            network_number=44,
+            latitude=0,
+            longitude=0,
+            status=Node.NodeStatus.ACTIVE,
+        )
+        node.save()
+
+        install = Install(**self.sample_install_copy)
+        install.node = node
+        install.save()
+
+        self.assertEqual(len(request_mocker.request_history), 1)
+        self.assertEqual(
+            request_mocker.request_history[0].url,
+            "http://example.com/test-url",
+        )
+        self.assertEqual(
+            json.loads(request_mocker.request_history[0].text),
+            {
+                "node": install.install_number,
+                "userNode": install.install_number,
+                "email": "john.smith@example.com",
+                "name": "John Smith",
+                "subject": f"NYC Mesh {install.install_number} Rooftop Install",
+                "message": f"date: 2022-02-27\r\nnode: {install.install_number}\r\nname: John Smith\r\nemail: john.smith@example.com\r\nphone: +1 555-555-5555\r\nlocation: 3333 Chom St, Brooklyn NY, 11111\r\nrooftop: Rooftop install\r\nagree to ncl: True",
+                "phone": "+1 555-555-5555",
+                "location": "3333 Chom St, Brooklyn NY, 11111",
+                "rooftop": "Rooftop install",
+                "existingNetworkNumber": "44",
                 "ncl": True,
                 "ip": "*.*.*.*",
                 "locale": "en",
