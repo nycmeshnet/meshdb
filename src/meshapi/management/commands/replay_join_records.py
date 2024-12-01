95be6c46722b1a5d165b0636a9dc14e05202de2a
Willard Nilges <willardnilges@gmail.com>
2024-12-01 17:25:58 -0500

    Add support for pre, post join records (#746)
    * fix command line
    
    * Add discrepancy checking and update command
    
    * Upload successful replays to another log
    
    * Write any replay attempt to the replays prefix
    
    * Remove replayed prefix
    
    * More test coverage
    
    * Handle 409's. Will probably need another test
    
    * Force user to make valid choice and lint

--- src/meshapi/management/commands/replay_join_records.py
+++ src/meshapi/management/commands/replay_join_records.py
@@ -1,3 +1,4 @@
+import logging
 from argparse import ArgumentParser
 from dataclasses import asdict, fields
 from datetime import datetime, timedelta, timezone
@@ -6,13 +7,14 @@ from typing import Any
 from django.core.management.base import BaseCommand
 from prettytable import PrettyTable
 
-from meshapi.util.join_records import JOIN_RECORD_PREFIX, JoinRecord, JoinRecordProcessor
+from meshapi.util.join_records import JoinRecord, JoinRecordProcessor, SubmissionStage
 from meshapi.views.forms import JoinFormRequest, process_join_form
 
 
 class Command(BaseCommand):
     help = "Replay join form submissions that we may not have accepted properly."
     "Defaults to viewing. Pass --write to write the records to MeshDB."
+    hidden_fields = ["version", "uuid", "replayed"]
 
     def add_arguments(self, parser: ArgumentParser) -> None:
         parser.add_argument(
@@ -30,29 +32,55 @@ class Command(BaseCommand):
         parser.add_argument(
             "--since",
             type=lambda s: datetime.fromisoformat(s + "Z"),  # Adding the Z makes this a tz-aware datetime
-            help="Show records submitted since this date and time (UTC, 24-Hour) (%Y-%m-%d %H:%M:%S)",
+            help="Show records submitted since this date and time (UTC, 24-Hour) (yyyy-mm-ddTHH:MM:SS)",
+        )
+
+        parser.add_argument(
+            "--all-columns",
+            action="store_true",
+            help=f"Display all information from the Join Record: {self.hidden_fields}",
         )
 
     def handle(self, *args: Any, **options: Any) -> None:
+        logging.info("Fetching Join Records...")
+
         p = JoinRecordProcessor()
 
         # Default to getting join records from 1 week ago unless otherwise specified
         since = options["since"] or self.past_week()
 
-        join_records = p.get_all(since)
+        # Ensure that the join records in pre-submission match the ones in post-submission
+        # This method will get both sets of records and supplement the post-submission
+        # records if any are missing.
+        consistent_join_records_dict = p.ensure_pre_post_consistency(since)
 
         table = PrettyTable()
         table.padding_width = 0
 
-        table.field_names = (key.name for key in fields(JoinRecord))
+        # These fields aren't really that relevant to human eyes.
+        if options["all_columns"]:
+            hidden_fields = []
+        else:
+            hidden_fields = self.hidden_fields
+        table.field_names = (key.name for key in fields(JoinRecord) if key.name not in hidden_fields)
 
-        for entry in join_records:
-            if (not options["all"]) and entry.install_number:
+        join_records_to_replay = {}
+
+        for uuid, record in consistent_join_records_dict.items():
+            if not options["all"]:
                 # Ignore submissions that are known good
-                join_records.remove(entry)
-                continue
+                if record.install_number:
+                    continue
 
-            table.add_row(asdict(entry).values())
+                # Don't bother replaying 400's. All we care about are 500's and nulls
+                if record.code and 400 <= int(record.code) and int(record.code) <= 499:
+                    continue
+
+            join_records_to_replay[uuid] = record
+            record_as_dict = asdict(record)
+            for k in hidden_fields:
+                record_as_dict.pop(k)
+            table.add_row(record_as_dict.values())
 
         if not options["all"]:
             print("The following Join Requests HAVE NOT been successfully submitted.")
@@ -62,30 +90,84 @@ class Command(BaseCommand):
         if not options["write"]:
             return
 
+        if not join_records_to_replay:
+            logging.warning("Found no join records to replay. Quitting")
+            return
+
         if not options["noinput"]:
             proceed_with_replay = input("Proceed with replay? (y/N): ")
-            if proceed_with_replay.lower() != "yes" and proceed_with_replay.lower() != "y":
+            if proceed_with_replay.lower() not in ["yes", "y"]:
                 print("Operation cancelled.")
                 return
 
         print("Replaying Join Records...")
 
-        for record in join_records:
+        for record in join_records_to_replay.values():
             # Make the request
             r = JoinFormRequest(
                 **{k: v for k, v in record.__dict__.items() if k in JoinFormRequest.__dataclass_fields__}
             )
             response = process_join_form(r)
+
+            print(f"{response.status_code} : {response.data}")
+
+            if response.status_code == 409:
+                print("Please confirm some information:")
+                print("Changed Info:")
+
+                confirmation_table = PrettyTable()
+                confirmation_table.padding_width = 0
+                confirmation_table.field_names = ["Field", "Original", "Suggested"]
+
+                for field, value in response.data["changed_info"].items():
+                    original_value = r.__dict__[field]
+                    confirmation_table.add_row([field, original_value, value])
+
+                print(confirmation_table)
+
+                if not options["noinput"]:
+                    # Trap the user until they make a valid choice
+                    while True:
+                        user_input = input("(A)ccept/(R)eject/(S)kip ?: ")
+                        if user_input in ["accept", "reject", "skip", "a", "r", "s"]:
+                            break
+                else:
+                    user_input = "accept"
+                    logging.warning("--no-input was specified, so auto-accepting")
+
+                if user_input.lower() in ["accept", "a"]:
+                    logging.info("Re-submitting with accepted changes...")
+                    r.__dict__.update(response.data["changed_info"])
+
+                    # If this doesn't work, then uh, skill issue.
+                    response = process_join_form(r)
+                    logging.info(f"Code: {response.status_code}")
+
+                elif user_input.lower() in ["reject", "r"]:
+                    logging.info("Rejecting changes and re-submitting...")
+                    r.__dict__.update({"trust_me_bro": True})
+                    record.trust_me_bro = True
+
+                    # If this doesn't work, then uh, skill issue.
+                    response = process_join_form(r)
+                    logging.info(f"Code: {response.status_code}")
+
+                elif user_input.lower() in ["skip", "s"]:
+                    logging.info("Skipping...")
+                    continue
+
             record.code = str(response.status_code)
             record.replayed += 1
             if response.data.get("install_number"):
                 record.install_number = response.data["install_number"]
+                logging.info("OK")
+            else:
+                logging.error(
+                    "Replay failed! Did not get an install number for "
+                    f"record: {JoinRecordProcessor.get_key(record, SubmissionStage.POST)}."
+                )
 
-            print(f"{response.status_code} : {response.data}")
-
-            # Upload info to S3
-            submission_datetime = datetime.fromisoformat(record.submission_time)
-            key = submission_datetime.strftime(f"{JOIN_RECORD_PREFIX}/%Y/%m/%d/%H/%M/%S.json")
+            key = JoinRecordProcessor.get_key(record, SubmissionStage.POST)
             p.upload(record, key)
 
     @staticmethod
--- src/meshapi/tests/test_replay_join_records.py
+++ src/meshapi/tests/test_replay_join_records.py
@@ -7,11 +7,18 @@ from django.test import TestCase
 from moto import mock_aws
 
 from meshapi.models.install import Install
-from meshapi.tests.sample_join_records import MOCK_JOIN_RECORD_PREFIX, basic_sample_join_records
+from meshapi.tests.sample_join_records import (
+    MOCK_JOIN_RECORD_PREFIX,
+    basic_sample_post_submission_join_records,
+    basic_sample_pre_submission_join_records,
+    sample_dont_replay_join_records_post,
+    sample_dont_replay_join_records_pre,
+)
 from meshapi.util.join_records import (
     JOIN_RECORD_BUCKET_NAME,
     JoinRecord,
     JoinRecordProcessor,
+    SubmissionStage,
     s3_content_to_join_record,
 )
 from meshapi.validation import NYCAddressInfo
@@ -23,60 +30,169 @@ from meshapi.validation import NYCAddressInfo
 # somehow from Russia), and maybe mock a MeshDB 500 that just ends in us putting
 # the data back.
 @mock_aws
+@patch("meshapi.util.join_records.JOIN_RECORD_PREFIX", MOCK_JOIN_RECORD_PREFIX)
 class TestReplayJoinRecords(TestCase):
     p = JoinRecordProcessor()
 
     def setUp(self) -> None:
+        print(JOIN_RECORD_BUCKET_NAME)
         self.p.s3_client.create_bucket(Bucket=JOIN_RECORD_BUCKET_NAME)
         self.p.flush_test_data()
+
+        # Load the samples into S3
+        for key, record in basic_sample_pre_submission_join_records.items():
+            self.p.upload(record, key)
+
+        for key, record in basic_sample_post_submission_join_records.items():
+            self.p.upload(record, key)
 
     def tearDown(self) -> None:
         self.p.flush_test_data()
 
-    @patch("meshapi.util.join_records.JOIN_RECORD_PREFIX", MOCK_JOIN_RECORD_PREFIX)
-    def test_get_all_since(self):
-        # Load the samples into S3
-        for key, record in basic_sample_join_records.items():
-            self.p.upload(record, key)
+    # I broke the help menu once upon a time so I need to make sure this works.
+    def test_help_works(self):
+        with self.assertRaises(SystemExit):
+            management.call_command("replay_join_records", "--help")
 
+    def test_list_records(self):
+        management.call_command("replay_join_records")
+
+    def test_get_all_since(self):
         records_since = self.p.get_all(since=datetime.fromisoformat("2024-10-01 00:00:00"))
 
-        self.assertEqual(len(records_since), 2)
+        self.assertEqual(len(records_since), 4)
 
         self.assertEqual(
-            basic_sample_join_records[f"{MOCK_JOIN_RECORD_PREFIX}/2024/10/28/12/34/56.json"], records_since[0]
+            basic_sample_post_submission_join_records[
+                f"{MOCK_JOIN_RECORD_PREFIX}/v3/post/2024/10/28/12/34/56/ec7b.json"
+            ],
+            records_since[0],
         )
         self.assertEqual(
-            basic_sample_join_records[f"{MOCK_JOIN_RECORD_PREFIX}/2024/10/30/12/34/57.json"], records_since[1]
+            basic_sample_post_submission_join_records[
+                f"{MOCK_JOIN_RECORD_PREFIX}/v3/post/2024/10/30/12/34/57/0490.json"
+            ],
+            records_since[1],
+        )
+        self.assertEqual(
+            basic_sample_post_submission_join_records[
+                f"{MOCK_JOIN_RECORD_PREFIX}/v3/post/2024/10/30/12/34/59/7cd3.json"
+            ],
+            records_since[2],
+        )
+        self.assertEqual(
+            basic_sample_post_submission_join_records[
+                f"{MOCK_JOIN_RECORD_PREFIX}/v3/post/2024/10/30/12/35/00/0f84.json"
+            ],
+            records_since[3],
         )
 
         records_since = self.p.get_all(since=datetime.fromisoformat("2024-10-29 00:00:00"))
-        self.assertEqual(len(records_since), 1)
+        self.assertEqual(len(records_since), 3)
 
         self.assertEqual(
-            basic_sample_join_records[f"{MOCK_JOIN_RECORD_PREFIX}/2024/10/30/12/34/57.json"], records_since[0]
+            basic_sample_post_submission_join_records[
+                f"{MOCK_JOIN_RECORD_PREFIX}/v3/post/2024/10/30/12/34/57/0490.json"
+            ],
+            records_since[0],
+        )
+        self.assertEqual(
+            basic_sample_post_submission_join_records[
+                f"{MOCK_JOIN_RECORD_PREFIX}/v3/post/2024/10/30/12/34/59/7cd3.json"
+            ],
+            records_since[1],
+        )
+        self.assertEqual(
+            basic_sample_post_submission_join_records[
+                f"{MOCK_JOIN_RECORD_PREFIX}/v3/post/2024/10/30/12/35/00/0f84.json"
+            ],
+            records_since[2],
         )
 
+    # This is just to make codecov happy
+    def test_s3_content_to_join_record(self):
+        sample_key = f"{MOCK_JOIN_RECORD_PREFIX}/v3/post/2024/11/01/11/33/49/0490.json"
+
+        # We're converting this...
+        sample_join_record_s3_content = json.dumps(
+            {
+                "first_name": "Jon",
+                "last_name": "Smith",
+                "email_address": "js@gmail.com",
+                "phone_number": "+1 585-475-2411",
+                "street_address": "197 Prospect Place",
+                "apartment": "1",
+                "city": "Brooklyn",
+                "state": "NY",
+                "zip_code": "11238",
+                "roof_access": True,
+                "referral": "I googled it.",
+                "ncl": True,
+                "trust_me_bro": False,
+                "version": 3,
+                "uuid": "1a55b949-0490-4b78-a2e8-10aea41d6f1d",
+                "code": "201",
+                "replayed": 0,
+                "install_number": 1002,
+            }
+        )
+
+        # To this.
+        sample_join_record: JoinRecord = JoinRecord(
+            first_name="Jon",
+            last_name="Smith",
+            email_address="js@gmail.com",
+            phone_number="+1 585-475-2411",
+            street_address="197 Prospect Place",
+            apartment="1",
+            city="Brooklyn",
+            state="NY",
+            zip_code="11238",
+            roof_access=True,
+            referral="I googled it.",
+            ncl=True,
+            trust_me_bro=False,
+            version=3,
+            uuid="1a55b949-0490-4b78-a2e8-10aea41d6f1d",
+            submission_time="2024-11-01T11:33:49",
+            code="201",
+            replayed=0,
+            install_number=1002,
+        )
+        join_record = s3_content_to_join_record(sample_key, sample_join_record_s3_content)
+        try:
+            self.assertEqual(join_record, sample_join_record, "Join Records do not match")
+        except AssertionError as e:
+            print(sample_join_record)
+            print(join_record)
+            raise e
+
     @patch("meshapi.management.commands.replay_join_records.Command.past_week")
-    @patch("meshapi.util.join_records.JOIN_RECORD_PREFIX", MOCK_JOIN_RECORD_PREFIX)
     @patch("meshapi.views.forms.geocode_nyc_address")
-    def test_replay_basic_join_records(self, mock_geocode_func, past_week_function):
+    def test_replay_join_records_with_write(self, mock_geocode_func, past_week_function):
+        # Pretend that it's halloween
         halloween_minus_one_week = datetime(2024, 10, 31, 8, 0, 0, 0) - timedelta(days=7)
         past_week_function.return_value = halloween_minus_one_week
+
+        # Mock return variables from Geocode API
         mock_geocode_func.side_effect = [
             NYCAddressInfo("197 Prospect Place", "Brooklyn", "NY", "11238"),
             ValueError("NJ not allowed yet!"),
+            NYCAddressInfo("99 Kane Street", "Brooklyn", "NY", "11231"),
+            # These next two are both for the 4th join record (Rachel Doe) that got 409'ed
+            NYCAddressInfo("99 Kane Street", "Brooklyn", "NY", "11231"),
+            NYCAddressInfo("99 Kane Street", "Brooklyn", "NY", "11231"),
         ]
-        # Load the samples into S3
-        for key, record in basic_sample_join_records.items():
-            self.p.upload(record, key)
 
         # Replay the records (this should get from the last week (halloween -7 days))
         management.call_command("replay_join_records", "--noinput", "--write")
 
-        records = self.p.get_all()
+        records = self.p.get_all(submission_prefix=SubmissionStage.POST)
 
-        self.assertEqual(2, len(records), "Got unexpected number of records in mocked S3 bucket.")
+        # After replaying, ensure we have the right number of post-submission records.
+        # The replay script should've only updated the ones that were already there,
+        # inserted the "pre" submission one that was missing, and ignored the 201.
+        self.assertEqual(5, len(records), "Got unexpected number of records in mocked S3 bucket.")
 
         # The first record should have been fine and dandy
         r = records[0]
@@ -110,56 +226,141 @@ class TestReplayJoinRecords(TestCase):
         self.assertEqual(2, r.replayed, "Did not get expected replay count.")
         self.assertEqual(None, r.install_number, "Install Number is not None.")
 
-    # This is just to make codecov happy
-    def test_s3_content_to_join_record(self):
-        sample_key = "dev-join-form-submissions/2024/11/01/11/33/49.json"
-
-        # We're converting this...
-        sample_join_record_s3_content = json.dumps(
-            {
-                "first_name": "Jon",
-                "last_name": "Smith",
-                "email_address": "js@gmail.com",
-                "phone_number": "+1 585-475-2411",
-                "street_address": "197 Prospect Place",
-                "apartment": "1",
-                "city": "Brooklyn",
-                "state": "NY",
-                "zip_code": "11238",
-                "roof_access": True,
-                "referral": "I googled it.",
-                "ncl": True,
-                "trust_me_bro": False,
-                "code": "201",
-                "replayed": 0,
-                "install_number": 1002,
-            }
+        # Pre-submission with the missing post-submission. This one
+        # should also work fine
+        r = records[2]
+        expected_code = "201"
+        self.assertEqual(
+            expected_code,
+            r.code,
+            f"Did not find correct replay code in mocked S3 bucket. Expected: {expected_code}, Got: {r.code}",
+        )
+        self.assertEqual(1, r.replayed, "Did not get expected replay count.")
+        install = Install.objects.get(install_number=r.install_number)
+        self.assertEqual("Benjamin Doe", install.member.name, "Did not get expected name for submitted install.")
+        self.assertEqual(
+            "99 Kane Street",
+            install.building.street_address,
+            "Did not get expected street address for submitted install.",
         )
 
-        # To this.
-        sample_join_record: JoinRecord = JoinRecord(
-            first_name="Jon",
-            last_name="Smith",
-            email_address="js@gmail.com",
-            phone_number="+1 585-475-2411",
-            street_address="197 Prospect Place",
-            apartment="1",
-            city="Brooklyn",
-            state="NY",
-            zip_code="11238",
-            roof_access=True,
-            referral="I googled it.",
-            ncl=True,
-            trust_me_bro=False,
-            submission_time="2024-11-01T11:33:49",
-            code="201",
-            replayed=0,
-            install_number=1002,
+        # Request was submitted while MeshDB was hard down. Should've gotten a 409
+        # and then a 201
+        r = records[3]
+        expected_code = "201"
+        self.assertEqual(
+            expected_code,
+            r.code,
+            f"Did not find correct replay code in mocked S3 bucket. Expected: {expected_code}, Got: {r.code}",
         )
-        join_record = s3_content_to_join_record(sample_key, sample_join_record_s3_content)
-        try:
-            self.assertEqual(join_record, sample_join_record, "Join Records do not match")
-        except AssertionError as e:
-            print(sample_join_record)
-            print(join_record)
-            raise e
+        self.assertEqual(1, r.replayed, "Did not get expected replay count.")
+        install = Install.objects.get(install_number=r.install_number)
+        self.assertEqual("Rachel Doe", install.member.name, "Did not get expected name for submitted install.")
+        self.assertEqual(
+            "99 Kane Street",
+            install.building.street_address,
+            "Did not get expected street address for submitted install.",
+        )
+
+        # Ensure we didn't touch the 201'ed request (Doesn't actually exist in the DB
+        # but ¯\_(ツ)_/¯)
+        r = records[4]
+        expected_code = "201"
+        self.assertEqual(
+            expected_code,
+            r.code,
+            f"Did not find correct replay code in mocked S3 bucket. Expected: {expected_code}, Got: {r.code}",
+        )
+        self.assertEqual(0, r.replayed, "Did not get expected replay count.")
+
+
+# codecov moment
+@mock_aws
+@patch("meshapi.util.join_records.JOIN_RECORD_PREFIX", MOCK_JOIN_RECORD_PREFIX)
+class TestReplayNoJoinRecords(TestCase):
+    p = JoinRecordProcessor()
+
+    def setUp(self) -> None:
+        self.p.s3_client.create_bucket(Bucket=JOIN_RECORD_BUCKET_NAME)
+        self.p.flush_test_data()
+
+    def tearDown(self) -> None:
+        self.p.flush_test_data()
+
+    def test_replay_no_join_records(self):
+        management.call_command("replay_join_records", "--noinput", "--write")
+
+
+# Skipping or rejecting changes
+@mock_aws
+@patch("meshapi.util.join_records.JOIN_RECORD_PREFIX", MOCK_JOIN_RECORD_PREFIX)
+class TestDontReplayJoinRecords(TestCase):
+    p = JoinRecordProcessor()
+
+    def setUp(self) -> None:
+        self.p.s3_client.create_bucket(Bucket=JOIN_RECORD_BUCKET_NAME)
+        self.p.flush_test_data()
+
+        # Load the samples into S3
+        for key, record in sample_dont_replay_join_records_pre.items():
+            self.p.upload(record, key)
+
+        for key, record in sample_dont_replay_join_records_post.items():
+            self.p.upload(record, key)
+
+    def tearDown(self) -> None:
+        self.p.flush_test_data()
+
+    @patch("meshapi.management.commands.replay_join_records.Command.past_week")
+    @patch("builtins.input")
+    def test_replay_join_records_skip(self, mocked_input, past_week_function):
+        halloween_minus_one_week = datetime(2024, 10, 31, 8, 0, 0, 0) - timedelta(days=7)
+        past_week_function.return_value = halloween_minus_one_week
+
+        # Force user input to skip
+        mocked_input.side_effect = ["yes", "skip"]
+
+        management.call_command("replay_join_records", "--noinput")
+
+        records = self.p.get_all(submission_prefix=SubmissionStage.POST)
+
+        self.assertEqual(1, len(records), "Got unexpected number of records in mocked S3 bucket.")
+
+        # If we skip it, we just won't touch it, so it should still be 503 and no
+        # replays
+        r = records[0]
+        expected_code = "503"
+        self.assertEqual(
+            expected_code,
+            r.code,
+            f"Did not find correct replay code in mocked S3 bucket. Expected: {expected_code}, Got: {r.code}",
+        )
+        self.assertEqual(0, r.replayed, "Did not get expected replay count.")
+
+    @patch("meshapi.management.commands.replay_join_records.Command.past_week")
+    @patch("builtins.input")
+    def test_replay_join_records_reject_changes(self, mocked_input, past_week_function):
+        halloween_minus_one_week = datetime(2024, 10, 31, 8, 0, 0, 0) - timedelta(days=7)
+        past_week_function.return_value = halloween_minus_one_week
+
+        # Force user input to skip
+        mocked_input.side_effect = ["yes", "reject"]
+
+        management.call_command("replay_join_records", "--write")
+
+        records = self.p.get_all(submission_prefix=SubmissionStage.POST)
+
+        self.assertEqual(1, len(records), "Got unexpected number of records in mocked S3 bucket.")
+
+        # Rejecting changes should still result in a 201
+        r = records[0]
+        expected_code = "201"
+        self.assertEqual(
+            expected_code,
+            r.code,
+            f"Did not find correct replay code in mocked S3 bucket. Expected: {expected_code}, Got: {r.code}",
+        )
+        self.assertEqual(1, r.replayed, "Did not get expected replay count.")
+        self.assertEqual(True, r.trust_me_bro, "Trust me bro should have been true.")
+        install = Install.objects.get(install_number=r.install_number)
+        self.assertEqual("Rachel Doe", install.member.name, "Did not get expected name for submitted install.")
