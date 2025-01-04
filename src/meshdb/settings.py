7db0687615e81d0a5d9772bba45e66dec09f6d25
Willard Nilges <willardnilges@gmail.com>
2025-01-04 12:41:03 -0500

    Now if only I could get my admin map to behave

--- src/meshdb/settings.py
+++ src/meshdb/settings.py
@@ -169,6 +169,7 @@ if DEBUG:
 
     CORS_ALLOWED_ORIGINS += [
         "http://127.0.0.1:3000",
+        "http://127.0.0.1:3001",
         "http://localhost:3000",
         "http://127.0.0.1:80",
         "http://localhost:80",
--- src/meshweb/static/admin/iframed.js
+++ src/meshweb/static/admin/iframed.js
@@ -13,23 +13,143 @@ function extractModel(inputString) {
 }
 
 
+function getCurrentTarget(url){
+    let path = url.replace(/^\/admin\/meshapi\//, "");
+    path = path.replace(/\/$/, "");
+
+    const [type, id, action] = path.split("/");
+    return [type, id, action];
+}
+
+// XXX (wdn): This function is plural, but it's actually just one node?
+async function getNewSelectedNodes(url){
+    //const [type, id, action] = getCurrentTarget(url);
+
+    const objectUUIDs = extractUUIDs(url);
+    const type = extractModel(url);
+
+    // Guard against looking up an empty UUID
+    if (objectUUIDs.length == 0) {
+      console.log("Found no UUID")
+      return null;
+    }
+    const id = objectUUIDs[0];
+
+    let nodeId = null;
+
+    if (type === "install") {
+        if (!id) return null;
+        const installResponse = await fetch(`/api/v1/installs/${id}/`);
+        if (!installResponse.ok) return null;
+        const install = await installResponse.json();
+        nodeId = install.install_number;
+    } else if (type === "node") {
+        if (!id) return null;
+        const nodeResponse = await fetch(`/api/v1/nodes/${id}/`);
+        if (!nodeResponse.ok) return null;
+        const node = await nodeResponse.json();
+        if (node.network_number) {
+            nodeId = node.network_number;
+        } else {
+            nodeId = node.installs[0].install_number;
+        }
+    } else if (type === "building") {
+        if (!id) return null;
+        const buildingResponse = await fetch(`/api/v1/buildings/${id}/`);
+        if (!buildingResponse.ok) return null;
+        const building = await buildingResponse.json();
+        if (building.primary_node && building.primary_node.network_number) {
+            nodeId = building.primary_node.network_number;
+        } else if (building.installs) {
+            nodeId = building.installs[0].install_number;
+        }
+    } else if (["device", "sector", "accesspoint"].indexOf(type) !== -1) {
+        if (!id) return null;
+        const deviceResponse = await fetch(`/api/v1/devices/${id}/`);
+        if (!deviceResponse.ok) return null;
+        const device = await deviceResponse.json();
+        nodeId = device.node.network_number;
+    } else if (type === "member") {
+        if (!id) return null;
+        const memberResponse = await fetch(`/api/v1/members/${id}/`);
+        if (!memberResponse.ok) return null;
+        const member = await memberResponse.json();
+        nodeId = member.installs.map(install => install.install_number).join("-");
+    } else if (type === "link") {
+        if (!id) return null;
+        const linkResponse = await fetch(`/api/v1/links/${id}/`);
+        if (!linkResponse.ok) return null;
+        const link = await linkResponse.json();
+
+        const device1Response = await fetch(`/api/v1/devices/${link.from_device.id}/`);
+        if (!device1Response.ok) return null;
+        const device1 = await device1Response.json();
+
+        const device2Response = await fetch(`/api/v1/devices/${link.to_device.id}/`);
+        if (!device2Response.ok) return null;
+        const device2 = await device2Response.json();
+
+        if (device1.node.network_number && device2.node.network_number) {
+            nodeId = `${device1.node.network_number}-${device2.node.network_number}`;
+        }
+    } else if (type === "los") {
+        if (!id) return null;
+        const losResponse = await fetch(`/api/v1/loses/${id}/`);
+        if (!losResponse.ok) return null;
+        const los = await losResponse.json();
+
+        let b1NodeId = null;
+        const buildingResponse1 = await fetch(`/api/v1/buildings/${los.from_building.id}/`);
+        if (!buildingResponse1.ok) return null;
+        const building1 = await buildingResponse1.json();
+        if (building1.primary_node && building1.primary_node.network_number) {
+            b1NodeId = building1.primary_node.network_number;
+        } else if (building1.installs) {
+            b1NodeId = building1.installs[0].install_number;
+        }
+
+        let b2NodeId = null;
+        const buildingResponse2 = await fetch(`/api/v1/buildings/${los.to_building.id}/`);
+        if (!buildingResponse2.ok) return null;
+        const building2 = await buildingResponse2.json();
+        if (building2.primary_node && building2.primary_node.network_number) {
+            b2NodeId = building2.primary_node.network_number;
+        } else if (building2.installs) {
+            b2NodeId = building2.installs[0].install_number;
+        }
+
+        if (b1NodeId && b2NodeId)  nodeId = `${b1NodeId}-${b2NodeId}`;
+    }
+
+    return nodeId ? `${nodeId}` : null;
+}
+
+
 // FIXME: Refreshes reset your location. Need to make sure that the iframe gets the refresh/forward/backward stuff. Check andrew's code.
 // FIXME: Also need to make sure that admin/members/uuid directs you to this iframe setup properly
 async function adminPanelLoaded() {
   const iframe_panel_url = document.getElementById("iframe_panel").contentWindow.location.href;
 
-  const objectUUIDs = extractUUIDs(iframe_panel_url)
-  const objectModel = extractModel(iframe_panel_url)
+  /*
+  const objectUUIDs = extractUUIDs(iframe_panel_url);
+  const objectModel = extractModel(iframe_panel_url);
+  */
+  const selectedNodes = await getNewSelectedNodes(iframe_panel_url);
 
-  document.getElementById("iframe_url").innerHTML = iframe_panel_url + " ---> " + objectModel + " || " + objectUUIDs;
+  document.getElementById("iframe_url").innerHTML = `${iframe_panel_url} ---> Selected Nodes: ${selectedNodes}`;
 
+  /*
   // Guard against looking up an empty UUID
   if (objectUUIDs.length == 0) {
-    console.log("No UUID");
+    console.log("No UUID")
+  }*/
+
+  if (selectedNodes === null) {
+    console.log("No node");
     return;
   }
 
-  console.log(objectUUIDs[0]);
+  /*
   // This is a dev token
   const token = "";
 
@@ -43,5 +163,10 @@ async function adminPanelLoaded() {
 
   const objectInfoJson = await objectInfo.json();
   console.log(objectInfoJson);
+  */
+
+  const selectedEvent = new Event("setMapNode");//, {detail: {selectedNodes: selectedNodes}});
+  selectedEvent.selectedNodes = selectedNodes;
+  document.getElementById("map_panel").contentWindow.top.postMessage(JSON.parse(JSON.stringify(selectedEvent)), "*");
 }
 
