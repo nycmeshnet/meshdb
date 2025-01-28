67af1a120cebdaccf878cb74aa5b45c774f067b2
Willard Nilges <willardnilges@gmail.com>
2025-01-27 19:15:20 -0500

    checkpoint

--- src/meshapi/management/commands/dedupe_history_entries.py
+++ src/meshapi/management/commands/dedupe_history_entries.py
@@ -16,30 +16,35 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args: Any, **options: Any) -> None:
-        self.deduplicate_history(Link.objects.all())
+        # self.deduplicate_history(Link.objects.all())
         self.deduplicate_history(Device.objects.all())
 
     def deduplicate_history(self, model_objects: models.QuerySet):
         for m in model_objects:
             logging.info(f"{m.id}, {m}")
-
-            # Delete history from each object
-            # Atomic block makes it go faster
+            # Delete history from each object. Atomic block makes it go faster
             with transaction.atomic():
                 try:
                     history = m.history.all()
                     # If there's no history, bail
                     if not history:
                         continue
-                    first_history = None
+                    # This is the history object that last changed something
+                    last_meaningful_history = None
                     for h in history:
-                        # Hacky way to preserve the first object
-                        if not first_history:
-                            first_history = h
+                        if not last_meaningful_history:
+                            last_meaningful_history = h
                             continue
-                        if not h.history_user_id:
-                            #logging.info(f"Deleting history record with user_id = None: {h}")
-                            h.delete()
+                        delta = last_meaningful_history.diff_against(
+                            h, 
+                            # foreign_keys_are_objs=foreign_keys_are_objs
+                        )
+                        print(delta)
+                        return
+                        #import pdb; pdb.set_trace()
+                        #if not h.history_user_id:
+                        #    #logging.info(f"Deleting history record with user_id = None: {h}")
+                        #    h.delete()
 
                 except Exception as e:
                     logging.exception(f"Could not get history for this link: {e}")
