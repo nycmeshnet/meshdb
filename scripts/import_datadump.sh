c00c762be98d99b048978133605eaab65e2e7785
Andrew Dickinson <andrewjd@amazon.com>
2025-03-03 21:50:55 -0500

    Fix dev import process

--- scripts/import_datadump.sh
+++ scripts/import_datadump.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-DOCKER_PG_COMMAND="docker exec -i meshdb-postgres-1 psql -U meshdb"
-DATA_DIR="./spreadsheet_data/"
+DOCKER_PG_COMMAND="docker exec -i meshdb-postgres-1 psql -U meshdb -d meshdb"
+DATA_DIR="./data/"
 tables=(
+"meshapi_building"
 "meshapi_los"
 "meshapi_link"
 "meshapi_accesspoint"
@@ -11,7 +12,6 @@ tables=(
 "meshapi_building_nodes"
 "meshapi_node"
 "meshapi_install"
-"meshapi_building"
 "meshapi_member"
 "meshapi_historicallos"
 "meshapi_historicallink"
@@ -32,40 +32,22 @@ if [ ! -d "$DATA_DIR" ]; then
 	exit 1
 fi
 
-for table_name in "${tables[@]}"
-do
-    if [ ! -e "spreadsheet_data/$table_name.sql" ]; then
-		echo "$table_name.sql is missing!"
-		exit 1
-	fi
-done
-
-# Don't need to create them.
-# XXX (willnilges): Do we want to have an option to dump the tables?
-#for table_name in "${tables[@]}"
-#do
-#    docker exec -i meshdb_postgres_1 pg_dump -U meshdb --table="$table_name" > "$table_name.sql"
-#done
+if [ ! -e "$DATA_DIR/full_dump.sql" ]; then
+		echo "full_dump.sql is missing!"
+    exit 1
+fi
 
 num_tables=${#tables[@]}
-
-# Yeet
-# XXX (willnilges): Would it be better to use manage.py?
 for ((i = num_tables - 1; i >= 0; i--));
 do
 	$DOCKER_PG_COMMAND -c "DROP TABLE IF EXISTS ${tables[i]} CASCADE"
 done
 
+
 # Import the new data
-for table_name in "${tables[@]}"
-do
-	cat "spreadsheet_data/$table_name.sql" | $DOCKER_PG_COMMAND
-done
+cat "$DATA_DIR/full_dump.sql" | $DOCKER_PG_COMMAND
+
 
 # Fix the auto numbering sequence for installs
 max_install_number=$(($(${DOCKER_PG_COMMAND} -c "SELECT MAX(install_number) FROM meshapi_install" -At) + 1))
 ${DOCKER_PG_COMMAND} -c "ALTER SEQUENCE meshapi_install_install_number_seq RESTART WITH ${max_install_number}"
-
-
-
-
