554833f78dee1ec23f567194c857e946c1824927
james-otten <jamesotten1@gmail.com>
2025-07-12 10:48:12 -0400

    add back offline

--- src/meshweb/views/index.py
+++ src/meshweb/views/index.py
@@ -17,6 +17,7 @@ def index(request: HttpRequest) -> HttpResponse:
         ("meshweb/volunteer.png", "Volunteer Tools"): [
             ("/admin", "Admin Panel"),
             ("/static/meshweb/meshdb.kml", "KML Download"),
+            ("/api/v1/geography/whole-mesh.kml", "KML Download (Offline)"),
             ("/explorer/play", "SQL Explorer"),
             (f"{settings.FORMS_URL}/nn-assign/", "NN Assign Form"),
             (f"{settings.FORMS_URL}/query/", "Query Form"),
